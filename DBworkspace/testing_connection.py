1199ffd341b68a863ac26ad8a2d7e1eb705f8ed9
spandanaa10 <73033072+spandanaa10@users.noreply.github.com>
2023-11-25 14:59:23 -0500

    Add files via upload
    Here is the file with the changing csv file to db and running a sql command

---
+++ DBworkspace/testing_connection.py
@@ -0,0 +1,29 @@
+import sqlite3
+import pandas as pd
+
+df = pd.read_csv('spotify-2023.csv', encoding='ISO-8859-1')
+df.columns = df.columns.str.strip()
+connection = sqlite3.connect('spotify-2023.db')
+
+# Establish a connection to your database
+#connection = sqlite3.connect("spotify-2023.db")
+cursor = connection.cursor()
+
+# Read SQL command from the file
+#with open('create_table.sql', 'r') as sql_file:
+#    sql_command = sql_file.read()
+sql_command = "CREATE TABLE Artist (artist_name VARCHAR(255) PRIMARY KEY,track_name VARCHAR(255));"
+# Execute the SQL command
+cursor.execute(sql_command)
+#print ("connected to databse") #uncomment to test connection
+# Commit changes and close the connection
+connection.commit()
+
+select_query = "SELECT * FROM Artist;"
+cursor.execute(select_query)
+table_contents = cursor.fetchall()
+for row in table_contents:
+    print(row)  # Display each row of the table
+
+connection.close()
+#df.to_sql()
